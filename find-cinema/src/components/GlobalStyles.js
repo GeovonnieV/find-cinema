a6a323d7b77ccc63fa910c1a0a8825f5a2da7afc
unknown <geovonnnievasquez@gmail.com>
2021-05-30 21:06:31 -0500

    cards

---
+++ find-cinema/src/components/GlobalStyles.js
@@ -0,0 +1,11 @@
+import {createdGlobalStyle} from "styled-components";
+
+const GlobalStyles = createdGlobalStyle` 
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+`;
